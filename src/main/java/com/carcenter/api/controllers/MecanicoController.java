34b7206a9e329c500d5446b92b5c39f2cf0b2303
Jorge Ramirez <j_wjorge@hotmail.com>
2021-05-17 17:34:50 -0500

    Se agrega plantillas error 500 y 404

--- src/main/java/com/carcenter/api/controllers/MecanicoController.java
+++ src/main/java/com/carcenter/api/controllers/MecanicoController.java
@@ -45,7 +45,7 @@ public class MecanicoController {
 		model.addAttribute("nav_active_grid_mec", "nav-link"); 
 		model.addAttribute("nav_active_grid_man", "nav-link");
 		
-		return "formMecanico";
+		return "asd";
 	}
 	
 	@PostMapping("/formMecanico")
